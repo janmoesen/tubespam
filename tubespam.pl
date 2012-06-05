63e6ff3f44237a2d7a0a588b4b9de8ca75463883
Jan Moesen <github.com@moesen.nu>
2012-06-05 10:49:36 +0200

    "Initial" commit
    This started as a copy of TwatSpam, and has been tweaked without
    proper version control. Oh, well.

---
+++ tubespam.pl
@@ -0,0 +1,61 @@
+# == WHAT
+# Show the title of linked YouTube movies.
+#
+# == WHO
+# Jan Moesen, 2012
+#
+# == INSTALL
+# Save it in ~/.irssi/scripts/ and do /script load tubespam.pl
+
+use strict;
+use Irssi;
+use JSON::XS;
+use LWP::Simple;
+use HTML::TokeParser;
+use vars qw($VERSION %IRSSI);
+
+$VERSION = '0.1';
+%IRSSI = (
+	authors     => 'Jan Moesen',
+	name        => 'TubeSpam',
+	description => 'Show the title of linked YouTube movies.',
+	license     => 'GPL',
+	url         => 'http://jan.moesen.nu/',
+	changed     => 'Tue Jun 05 10:45:07 +0200 2012',
+);
+
+sub tubespam_process_message {
+	my ($server, $msg, $target) = @_;
+
+	return unless $target =~ /^#(wijs|catena|lolwut)/;
+	my $video_id = 0;
+	if ($msg =~ m/https?:\/\/youtu\.be\/(.+)(?:\?.*)?$/) {
+		$video_id = $1;
+	} elsif ($msg =~ m/https?:\/\/(?:www\.)youtube\.com\/.*\bv=([^&)]+)/) {
+		$video_id = $1;
+	}
+	return unless $video_id;
+
+	my $video_url = "http://gdata.youtube.com/feeds/api/videos/$video_id?v=2&fields=title";
+	my $html = get($video_url); # Actually, this is XML, but it Should Work.
+	return unless $html;
+	my $parser = HTML::TokeParser->new(\$html);
+	$parser->get_tag('title');
+	my $title = $parser->get_token;
+	return unless $title;
+
+	$title = "$title->[1]";
+	my $message = "YouTube video: \"$title\"";
+	$server->command("msg $target $message");
+}
+
+Irssi::signal_add_last('message public', sub {
+	my ($server, $msg, $nick, $mask, $target) = @_;
+	Irssi::signal_continue($server, $msg, $nick, $mask, $target);
+	tubespam_process_message($server, $msg, $target);
+});
+Irssi::signal_add_last('message own_public', sub {
+	my ($server, $msg, $target) = @_;
+	Irssi::signal_continue($server, $msg, $target);
+	tubespam_process_message($server, $msg, $target);
+});
