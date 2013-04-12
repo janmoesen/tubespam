00ad86e9022be3d369f82970f57d81c2195e2bce
Jan Moesen <github.com@moesen.nu>
2013-04-12 10:51:24 +0200

    Remove #wijs* from the channel list

--- tubespam.pl
+++ tubespam.pl
@@ -27,7 +27,7 @@ $VERSION = '0.1';
 sub tubespam_process_message {
 	my ($server, $msg, $target, $nick) = @_;
 
-	return unless $target =~ /^#(fronteers|wijs|catena|lolwut)/;
+	return unless $target =~ /^#(fronteers|catena|lolwut)/i;
 
 	my $video_id = 0;
 	if ($msg =~ m/https?:\/\/youtu\.be\/([-\w]+)/) {
